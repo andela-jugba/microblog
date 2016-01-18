ba887f2f11d47d42acbbeeb4104c0ea6649088b5
Joshua Ugba <justjosh001@gmail.com>
2016-01-18 10:01:12 +0100

    Finish time localizations

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,12 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+from .momentjs import momentjs
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.jinja_env.globals['momentjs'] = momentjs
+
 db = SQLAlchemy(app)
 
 # setup simple LoginManager
